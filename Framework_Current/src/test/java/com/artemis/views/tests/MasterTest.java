5d11c072bdc7a71933d045203beeeb835ff714f7
MGEETHAN <mgeethan@maxis.com.my>
2022-07-04 16:26:48 +0530

    test

---
+++ Framework_Current/src/test/java/com/artemis/views/tests/MasterTest.java
@@ -0,0 +1,35 @@
+package com.artemis.views.tests;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.crossover.testrail.integration.TestRailRunner;
+import com.cucumber.listener.ExtentCucumberFormatter;
+
+import cucumber.api.CucumberOptions;
+
+@RunWith(TestRailRunner.class)
+@CucumberOptions(features = { "classpath:feature_files/" }, glue = { "com.artemis.views.stepdefinition", 
+		"com.artemis.views.testrail.stepdefinition"  }, plugin = { "pretty", "json:target/cucumber",
+		"com.cucumber.listener.ExtentCucumberFormatter" })
+
+public class MasterTest {
+	@BeforeClass
+	public static void setup() {
+		// Initiates the extent report and generates the output in the
+		// output/Run_<unique timestamp>/report.html file by default.
+		ExtentCucumberFormatter.initiateExtentCucumberFormatter();
+
+		// User can add the system information as follows
+		ExtentCucumberFormatter.addSystemInfo("Selenium version", "v2.53.0");
+
+		// Also you can add system information using a hash map
+		final Map<String, String> systemInfo = new HashMap<String, String>();
+		systemInfo.put("Cucumber version", "v1.2.4");
+		systemInfo.put("Extent Cucumber Reporter version", "v1.1.1");
+		ExtentCucumberFormatter.addSystemInfo(systemInfo);
+	}
+}
